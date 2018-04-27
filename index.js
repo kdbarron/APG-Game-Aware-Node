e9c1afb69f2619161f7644889f0fbad7b1a0a3fe
Phan <bh7819@gmail.com>
2018-04-26 23:34:28 -0400

    first push

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'views/Client/website')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.sendFile( path.join(__dirname + '/views/Client/website/game.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
