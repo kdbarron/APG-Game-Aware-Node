63ac41068a321f51aaf89e484f4aa5cf77fd7276
Phan <bh7819@gmail.com>
2018-04-27 03:16:34 -0400

    d

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const app = express();
-const metaDirectory = path.join(__dirname + '/public/TestTraffic');
+const metaDirectory = __dirname + '/TestTraffic';
 const gameIndexPath = path.join(__dirname + '/views/Client/website/game.html')
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
